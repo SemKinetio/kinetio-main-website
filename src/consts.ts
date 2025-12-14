ae146a9ccd4b94cd262687c88e4dd50586a4fd58
HoussemSioud95 <houssem@kinetio.ai>
2025-12-14 16:47:14 +0100

    initial kinetio setup

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,13 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Astro Blog";
-export const SITE_DESCRIPTION = "Welcome to my website!";
+export const SITE_TITLE = "KINETIO";
+export const SITE_DESCRIPTION = "Next-generation AI movement intelligence.";
+
+export const CONTACT_EMAILS = {
+  INFO: "info@kinetio.ai",
+  SUPPORT: "support@kinetio.ai",
+  CONTACT: "contact@kinetio.ai",
+};
+
+export const CONTACT_ADDRESS = "Berlin, Germany";
